40db0aeea67cf3570a42f7679cd97d76c41b1694
rohit <rohit@DESKTOP-F16QUPD>
2020-07-11 09:43:47 +0530

    Final Commit using Cascade "Update"

--- Cascade/src/com/cascade/test/CASCADETest.java
+++ Cascade/src/com/cascade/test/CASCADETest.java
@@ -31,39 +31,48 @@ public class CASCADETest {
 			session=sessionFactory.openSession();
 			transaction=session.beginTransaction();
 			
-			assignedComplaints=new HashSet<>();
 			
+			  assignedComplaints=new HashSet<>();
+			  
+			  complaint=new Complaint();
+			  
+			/*
+			 * complaint.setTitle("Phone not switching on");
+			 * complaint.setDescription("Not Working"); complaint.setPriority(1);
+			 * complaint.setSeverity(1); complaint.setReportedDate(new Date());
+			 * complaint.setStatus("open");
+			 * 
+			 * // without cascade save-update we should save the compliant
+			 * 
+			 * assignedComplaints.add(complaint);
+			 * 
+			 * complaint.setTitle("Screen Damaged");
+			 * complaint.setDescription("Physical Damaged"); complaint.setPriority(2);
+			 * complaint.setSeverity(3); complaint.setReportedDate(new Date());
+			 * complaint.setStatus("open");
+			 * 
+			 * assignedComplaints.add(complaint);
+			 * 
+			 * csr=new Csr();
+			 * 
+			 * csr.setFullName("Jitesh Sonar"); csr.setDesignation("Junior");
+			 * csr.setExperience(2); csr.setMobileNo("123456");
+			 * csr.setAssignedComplaints(assignedComplaints); session.save(csr);
+			 */
+			 
+			
+			csr=session.get(Csr.class, 1);
 			complaint=new Complaint();
-			
-			complaint.setTitle("Phone not switching on");
-			complaint.setDescription("Not Working");
-			complaint.setPriority(1);
-			complaint.setSeverity(1);
+			complaint.setTitle("SIgnal Drop");
+			complaint.setDescription("");
+			complaint.setPriority(9);
+			complaint.setSeverity(8);
 			complaint.setReportedDate(new Date());
-			complaint.setStatus("open");
+			complaint.setStatus("urgent");
+			csr.getAssignedComplaints().add(complaint);
+			session.update(csr);
 			
-			// without cascade save-update we should save the compliant
-		
-			assignedComplaints.add(complaint);
 			
-			complaint.setTitle("Screen Damaged");
-			complaint.setDescription("Physical Damaged");
-			complaint.setPriority(2);
-			complaint.setSeverity(3);
-			complaint.setReportedDate(new Date());
-			complaint.setStatus("open");
-			
-			assignedComplaints.add(complaint);
-			
-			csr=new Csr();
-			
-			csr.setFullName("Jitesh Sonar");
-			csr.setDesignation("Junior");
-			csr.setExperience(2);
-			csr.setMobileNo("123456");
-			csr.setAssignedComplaints(assignedComplaints);
-			
-			session.save(csr);
 			
 			flag=true;
 		}
