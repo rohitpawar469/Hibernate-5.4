8c62b31de3cc281df6ce4e91918e64419f51045e
rohit <rohit@DESKTOP-F16QUPD>
2020-07-11 19:33:57 +0530

    Final Commit Using Sequence JPA-ID

---
+++ JPA-ID_Sequence/src/com/jpa/helper/EntitiyManagerFactoryRegistry.java
@@ -0,0 +1,53 @@
+package com.jpa.helper;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntitiyManagerFactoryRegistry
+{
+
+	private static EntityManagerFactory mysqlFactory; 
+	private static EntityManagerFactory oracleFactory;
+	
+	static
+	{
+		
+		try
+		{
+			
+			
+				mysqlFactory=Persistence.createEntityManagerFactory("mysql");	
+			
+				oracleFactory=Persistence.createEntityManagerFactory("oracle");	
+			
+			
+		} 
+		catch (Throwable t)
+		{
+			t.printStackTrace();
+			throw t;
+		}
+		
+	}
+
+	public static final EntityManagerFactory getMysqlFactory() {
+		return mysqlFactory;
+	}
+
+	public static final EntityManagerFactory getOracleFactory() {
+		return oracleFactory;
+	}
+	
+	public static void close()
+	{
+		if(mysqlFactory!=null)
+		{
+			mysqlFactory.close();
+		}
+		if(oracleFactory!=null)
+		{
+			oracleFactory.close();
+		}
+	}
+	
+}
