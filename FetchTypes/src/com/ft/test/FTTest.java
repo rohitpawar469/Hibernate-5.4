dc090329942dcba6dfaa3948d9c297543ee512dc
rohit <rohit@DESKTOP-F16QUPD>
2020-07-13 20:55:55 +0530

    For fetching data using join

--- FetchTypes/src/com/ft/test/FTTest.java
+++ FetchTypes/src/com/ft/test/FTTest.java
@@ -32,37 +32,39 @@ public class FTTest {
 			
 			feedbacks=new HashSet<>();
 			
-			product=new Product();
-			product.setProductName("Microwave Owen");
-			product.setManufacturer("LG");
-			product.setDescription(" Full Convection");
-			product.setPrice(45000);
-
-			feedback=new Feedback();
-			feedback.setComments("Superb Device");
-			feedback.setRating(5);
-			feedback.setFeedbackPostedDate(new Date());
-			feedback.setFeedbackUserName("Sneha");
-			feedback.setProduct(product);
-			session.save(feedback);
+			/*
+			 * product=new Product(); product.setProductName("Microwave Owen");
+			 * product.setManufacturer("LG"); product.setDescription(" Full Convection");
+			 * product.setPrice(45000);
+			 * 
+			 * feedback=new Feedback(); feedback.setComments("Superb Device");
+			 * feedback.setRating(5); feedback.setFeedbackPostedDate(new Date());
+			 * feedback.setFeedbackUserName("Sneha"); feedback.setProduct(product);
+			 * session.save(feedback);
+			 * 
+			 * feedback=new Feedback(); feedback.setComments("Poor 360 Degree heating");
+			 * feedback.setRating(2); feedback.setFeedbackPostedDate(new Date());
+			 * feedback.setFeedbackUserName("Neha"); feedback.setProduct(product);
+			 * session.save(feedback);
+			 * 
+			 * feedback=new Feedback(); feedback.setComments("Not heating MAX tem");
+			 * feedback.setRating(3); feedback.setFeedbackPostedDate(new Date());
+			 * feedback.setFeedbackUserName("Sharvari"); feedback.setProduct(product);
+			 * session.save(feedback);
+			 */
 			
-			feedback=new Feedback();
-			feedback.setComments("Poor 360 Degree heating");
-			feedback.setRating(2);
-			feedback.setFeedbackPostedDate(new Date());
-			feedback.setFeedbackUserName("Neha");
-			feedback.setProduct(product);
-			session.save(feedback);
+			/*
+			 * product=session.get(Product.class, 5); System.out.println("Product Name :"
+			 * +product.getProductName());
+			 * 
+			 * feedbacks=product.getFeedbacks(); for (Feedback feedback1 : feedbacks) {
+			 * System.out.println(feedback1); }
+			 */
 			
-			feedback=new Feedback();
-			feedback.setComments("Not heating MAX tem");
-			feedback.setRating(3);
-			feedback.setFeedbackPostedDate(new Date());
-			feedback.setFeedbackUserName("Sharvari");
-			feedback.setProduct(product);
-			session.save(feedback);
 			
-		
+			product=session.get(Product.class, 6);
+			System.out.println("Product Name :"+product.getProductName());
+			
 			
 			flag=true;
 		} finally {
