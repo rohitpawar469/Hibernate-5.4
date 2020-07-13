bd09be3703b792d3660056d5a6a155d86c49d509
rohit <rohit@DESKTOP-F16QUPD>
2020-07-13 20:26:05 +0530

    Revised Product Pojo Class

--- FetchTypes/src/com/ft/entities/Product.java
+++ FetchTypes/src/com/ft/entities/Product.java
@@ -2,40 +2,57 @@ package com.ft.entities;
 
 import java.io.Serializable;
 
-public class Product implements Serializable
-{
+public class Product implements Serializable {
 	protected int productNo;
 	protected String productName;
 	protected String description;
 	protected String manufacturer;
 	protected double price;
-	
-	
+
 	public int getProductNo() {
 		return productNo;
 	}
+
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+
 	public String getProductName() {
 		return productName;
 	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
 	public String getDescription() {
 		return description;
 	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	public String getManufacturer() {
 		return manufacturer;
 	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
 	public double getPrice() {
 		return price;
 	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 	@Override
 	public String toString() {
 		return "Product [productNo=" + productNo + ", productName=" + productName + ", description=" + description
 				+ ", manufacturer=" + manufacturer + ", price=" + price + "]";
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
